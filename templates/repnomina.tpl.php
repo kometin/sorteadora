6549e357dcb5ee8a21744ba6b31c16b543e43855
Christian Baltazar <cbaltazarc@guanajuato.gob.mx>
2018-02-01 12:48:51 -0600

    x

--- templates/repnomina.tpl.php
+++ templates/repnomina.tpl.php
@@ -6,30 +6,57 @@
 
 <?function Script($context){?>
 <script type ="text/javascript">
-    var grid;
+    var grid_det, grid_con;
     $(function(){
-        <?=setGrid("grid", $context->params, true)?>
-
+        <?=setGrid("grid_det", $context->params['DET'], true)?>
+        <?=setGrid("grid_con", $context->params['CON'], true)?>
+                
     });
 </script>
 <?}?>
 
 <?function Body($context){?>
+<ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#details" role="tab" data-toggle="tab">Reporte detallado</a></li>
+        <li role="presentation" class=""><a href="#concentrade" role="tab" data-toggle="tab">Reporte concentrado</a></li>
+       
+    </ul>
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane fade in active" id="details">
+            <table width="100%"  cellpadding="0" cellspacing="0">		
+                <tr>
+                     <td id="pager_grid_det"></td>
+                </tr>
+                <tr>
+                     <td><div id="infopage_grid_det" style =""></div></td>
+                </tr>
+                <tr>
+                     <td><div id="grid_det" style ="height: 500px; width: 100%"></div></td>
+                </tr>
+                <tr>
+                     <td class = "RowCount"></td>
+                </tr>
+            </table>
+        </div>
+        <div role="tabpanel" class="tab-pane fade in " id="concentrade">
+            <table width="100%"  cellpadding="0" cellspacing="0">		
+                <tr>
+                    <td id="pager_grid_con"></td>
+               </tr>
+               <tr>
+                    <td><div id="infopage_grid_con" style =""></div></td>
+               </tr>
+               <tr>
+                    <td><div id="grid_con" style ="height: 500px; width: 100%"></div></td>
+               </tr>
+               <tr>
+                    <td class = "RowCount"></td>
+               </tr>
+           </table>
+        </div>
+    </div>
+        
 
-<table width="100%"  cellpadding="0" cellspacing="0">		
-    <tr>
-         <td id="pager_grid"></td>
-    </tr>
-    <tr>
-         <td><div id="infopage_grid" style =""></div></td>
-    </tr>
-    <tr>
-         <td><div id="grid" style ="height: 500px; width: 100%"></div></td>
-    </tr>
-    <tr>
-         <td class = "RowCount"></td>
-    </tr>
-</table>
 
 <?}?>
 
